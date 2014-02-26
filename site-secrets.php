fe433f85a8f9f4879f179912c512ac864dd14a0d
Daniel Bachhuber <d@danielbachhuber.com>
2014-02-26 13:48:18 -0800

    On `template_redirect`, check whether the user has permission to view the blog. If not, redirect to login

--- site-secrets.php
+++ site-secrets.php
@@ -18,9 +18,52 @@ class Site_Secrets {
 
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new Site_Secrets;
+			self::$instance->setup_actions();
 		}
 
 		return self::$instance;
+	}
+
+	/**
+	 * Set up our WordPress integrations
+	 */
+	private function setup_actions() {
+
+		add_action( 'template_redirect', array( $this, 'action_template_redirect' ) );	
+
+	}
+
+	/**
+	 * Can the user view this site?
+	 * 
+	 * @return bool
+	 */
+	private function user_can_view() {
+
+		if ( is_user_logged_in() ) {
+
+			$requires_membership = apply_filters( 'site_secrets_requires_membership', true );
+			if ( $requires_membership && ! is_user_member_of_blog() ) {
+				return false;
+			}
+
+			return true;
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * If the user doesn't have permission to view this site, redirect to login
+	 */
+	public function action_template_redirect() {
+		global $pagenow;
+
+		if ( 'wp-login.php' !== $pagenow && ! $this->user_can_view() ) {
+			auth_redirect();
+		}
+
 	}
 
 }
