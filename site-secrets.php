351f0b2f4a0bb18e106c6b72a4606bc4b9992773
Daniel Bachhuber <d@danielbachhuber.com>
2014-02-26 13:40:40 -0800

    Plugin scaffold

--- site-secrets.php
+++ site-secrets.php
@@ -2,11 +2,33 @@
 /*
 Plugin Name: Site Secrets
 Version: 0.1-alpha
-Description: PLUGIN DESCRIPTION HERE
-Author: YOUR NAME HERE
-Author URI: YOUR SITE HERE
-Plugin URI: PLUGIN SITE HERE
+Description: Keep your WordPress site private.
+Author: Daniel Bachhuber
+Author URI: http://danielbachhuber.com
+Plugin URI: http://insidethunderdome.com
 Text Domain: site-secrets
 Domain Path: /languages
 */
 
+class Site_Secrets {
+
+	private static $instance;
+
+	public static function get_instance() {
+
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new Site_Secrets;
+		}
+
+		return self::$instance;
+	}
+
+}
+
+/**
+ * Load the plugin
+ */
+function Site_Secrets() {
+	return Site_Secrets::get_instance();
+}
+add_action( 'plugins_loaded', 'Site_Secrets' );
